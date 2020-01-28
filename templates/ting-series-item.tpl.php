aa54d64aca60683f3d9a2b6d38b64004bb8404b4
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-01-28 12:24:30 +0100

    Added sorting

---
+++ templates/ting-series-item.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<div class="ting-object view-mode-teaser imagestyle-ding-list-medium clearfix">
+   <div class="inner">
+   <?php print $cover; ?>
+   </div>
+   <div class="field field-name-ting-title field-type-ting-title field-label-hidden">
+      <h2><?php print $title; ?></h2>
+   </div>
+   <div class="ting-series-title"><?php print $series_title; ?></div>
+ </div>
+ 
