8d28d233fe3e0c29bc0b26b199312e97ec687c0b
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-02-19 12:03:50 +0100

    Handling series description series

--- templates/ting-series-item.tpl.php
+++ templates/ting-series-item.tpl.php
@@ -6,9 +6,9 @@
    <div class="inner">
    <?php print $cover; ?>
    </div>
-   <div class="field field-name-ting-title field-type-ting-title field-label-hidden">
+<!--   <div class="field field-name-ting-title field-type-ting-title field-label-hidden">
       <h2><?php print $title; ?></h2>
-   </div>
+   </div>-->
    <div class="ting-series-title"><?php print $series_title; ?></div>
  </div>
  
